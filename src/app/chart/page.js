b5c9e1d19dd33de35d6aac9704b073e63320718e
unknown <yosefeid058@gmail.com>
2024-05-24 19:30:44 +0300

    dash

--- src/app/chart/page.js
+++ src/app/chart/page.js
@@ -1,19 +1,14 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-// import Chart from 'react-apexcharts'
-
 import dynamic from 'next/dynamic';
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
+
 const Donut = () => {
-
-
   useEffect(() => {
     document.title = 'ألتقارير'
-    // setChartData()
   },[])
- 
 
   const [chartData, setChartData] = useState({
     options1: {
@@ -22,52 +17,50 @@ const Donut = () => {
       chart: {
         fontFamily: 'Helvetica, Arial, sans-serif', // Setting the font style for the chart
         foreColor: '#333333', // Setting the font color for the chart
-        fontSize: '30px', // Setting the font size for the chart
-
+        fontSize: '30px' // Setting the font size for the chart
       }
     },
-    series1: [44, 55, 41, 17, 44],
-  })
-
+    series1: [44, 55, 41, 17, 44], // Data for the first chart
+    options2: {
+      colors: ['#FF4560', '#00E396', '#4CAF50', '#008FFB', '#775DD0'], // Custom colors for the second chart
+      labels: ['F', 'G', 'H', 'I', 'J'],
+      chart: {
+        fontFamily: 'Helvetica, Arial, sans-serif', // Setting the font style for the chart
+        foreColor: '#333333', // Setting the font color for the chart
+        fontSize: '30px' // Setting the font size for the chart
+      }
+    },
+    series2: [21, 59, 75, 20, 20], // Data for the second chart
+    options3: {
+      colors: ['#FF6347', '#4682B4', '#32CD32', '#FFD700', '#6A5ACD'], // Custom colors for the third chart
+      labels: ['K', 'L', 'M', 'N', 'O'],
+      chart: {
+        fontFamily: 'Helvetica, Arial, sans-serif', // Setting the font style for the chart
+        foreColor: '#333333', // Setting the font color for the chart
+        fontSize: '30px' // Setting the font size for the chart
+      }
+    },
+    series3: [65, 40, 80, 30, 70], // Data for the third chart
+    options4: {
+      colors: ['#20B2AA', '#FF4500', '#800080', '#00CED1', '#C71585'], // Custom colors for the fourth chart
+      labels: ['P', 'Q', 'R', 'S', 'T'],
+      chart: {
+        fontFamily: 'Helvetica, Arial, sans-serif', // Setting the font style for the chart
+        foreColor: '#333333', // Setting the font color for the chart
+        fontSize: '30px' // Setting the font size for the chart
+      }
+    },
+    series4: [50, 60, 70, 40, 50] // Data for the fourth chart
+  });
 
   return (
-    <div>
-      {
-        // setTimeout(() => {
-          <Chart options={chartData.options1} series={chartData.series1} type="donut" width="380" />
-        // }, 0)
-      }
+    <div className='w-full h-[90vh] flex items-center justify-center'>
+      <Chart options={chartData.options1} series={chartData.series1} type="donut" width="380" />
+      <Chart options={chartData.options2} series={chartData.series2} type="donut" width="380" />
+      <Chart options={chartData.options3} series={chartData.series3} type="donut" width="380" />
+      <Chart options={chartData.options4} series={chartData.series4} type="donut" width="380" />
     </div>
-  )
+  );
 }
 
-export default Donut
-
-
-// class Donut extends Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       options: {},
-//       series: [44, 55, 41, 17, 15],
-//       labels: ['A', 'B', 'C', 'D', 'E']
-//     }
-//   }
-
-//   render() {
-
-//     return (
-//       <div className="w-full h-[80vh] flex items-center justify-center flex-wrap gap-9 mt-10">
-//         <Chart options={this.state.options} series={this.state.series} type="donut" width="380" />
-//         <Chart options={this.state.options} series={this.state.series} type="donut" width="380" />
-//         <Chart options={this.state.options} series={this.state.series} type="donut" width="380" />
-//         <Chart options={this.state.options} series={this.state.series} type="donut" width="380" />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Donut;
-
+export default Donut;
