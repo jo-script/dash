908053e0c2eb3e38f33248ef7526640dd50ee9c7
unknown <yosefeid058@gmail.com>
2024-05-25 09:01:20 +0300

    dash

--- src/app/edit-products/[id]/page.js
+++ src/app/edit-products/[id]/page.js
@@ -13,13 +13,13 @@ const EditProduct = ({ params }) => {
   }
 
   return (
-    <div className="w-full mx-auto p-4 flex items-center flex-col gap-5" style={{ direction: 'rtl' }}>
+    <div className="w-full mx-auto p-4 flex items-center flex-col gap-5 max-[800px]:p-1" style={{ direction: 'rtl' }}>
 
-      <div className='w-[60%] h-[350px] rounded-lg border flex item-center justify-center mt-3'>
+      <div className='w-[60%] max-[800px]:w-[80%] h-[350px] max-[800px]:h-auto rounded-lg border flex item-center justify-center mt-3'>
         <Image src={product.img} alt={product.name} />
       </div>
       {/* from edit product*/}
-      <form className="mt-4 w-[60%]">
+      <form className="mt-4 w-[60%] max-[800px]:w-[90%]">
         <div className="mb-4 w-full">
           <label className="block text-[#224971] text-sm font-bold mb-2" htmlFor="nameArabic">اسم المنتج</label>
           <input type="text" id="nameArabic" defaultValue={product.nameArabic} placeholder="ادخل اسم المنتج" className=" w-full appearance-none border border-[#22497173] text-gray-600 rounded-md  py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" />
