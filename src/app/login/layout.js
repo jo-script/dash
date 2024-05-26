e04a80f00908f30b6df37cff5c4895d2fb9521ea
unknown <yosefeid058@gmail.com>
2024-05-26 20:38:32 +0300

    dash

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      {children}
+      <body className={tajawal.className}>{children}</body>
     </html>
   );
 }
